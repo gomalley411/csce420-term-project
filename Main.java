caa1e5bd21d9549a85e2892c4221db0b774cb480
Sophia S <spswanberg44@gmail.com>
2021-10-26 17:03:39 -0500

    added outline in main. working on ready queue

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 import java.io.*;
 import java.util.*;
+import java.lang.Math;
+import Java.util.ArrayList;
+import Java.util.LinkedList;
 /*
 Group 8
 Sophia S
@@ -11,12 +14,63 @@ George O
 public class main{
     public static void main(String[] args){
         /**Sophia S. Last Modified Date: 10/25/21 */
-        Process p0 = new Process();
-        System.out.println("p0 created.");
-        p0.printProcess();
-        p0.setArr(1);
-        p0.setBurst(10);
-        p0.setExit(13);
-        p0.printProcess();
+        /*Process p0 = new Process(1, 5, 10, 0, 0, 1);
+        p0.printProcess();*/
+        
+        for(int i = 0; i < 4; i++){
+            int arrival = (int)(Math.random()*6);
+            int burst = (int)(Math.random()*10) + 1;
+            int iburst = (int)(Math.random()*20) + 1;
+            //System.out.println("Arrival: "+ arrival);
+            //System.out.println("burst: "+ burst);
+            //System.out.println("I/O burst: "+iburst);
+        }
+        
+
+
+
+
+
+
+        /**Step 1: Create Processes */
+        /**CPU Burst times should be in range 1-10 */
+        /**I/O Burst times should be in range 11-21 */
+
+        /**Step 2: Sort processes based on arrival times 
+            -If two processes come in at the same time, whichever
+            PID is closer to 0 will be put in front
+            -Round robin priority is based off of this sort*/
+
+        /**Step 3: Enter the ready queue */
+        /**We need to determine the size of the ready queue*/
+
+        /**Step 4: The first process in line will get the CPU */
+
+        /**Step 5: Run the process in the time quantum.
+            Finished? Move to terminated queue
+            Still working? Move to waiting queue to process I/O wait */
+        
+        /**New list:
+        All newly SORTED processes will go here. */
+
+        /**Ready list:
+        All CPU bound processess will be in this queue. The head of this list will be moved into 
+        the running queue. */
+
+        /**Running list:
+        This is where the process burst time will be decremented. If the time quantum is 4, then the 
+        process will have (burst time - 4) 
+        Finished? Record exit time*/
+
+        /**Waiting list:
+        This is where the process will go when it's not finished executing. It will stay in the wait queue
+        until it's I/O burst time variable has been decrememented to 0. */
+
+        /**Terminated list:
+        To enter this list, process burst time must be 0 and the process exit time must be recorded */
+
+        /**Notes:
+        This model will continue running until the size of the terminated list is equal to the number of 
+        processes created.  */
     }
 }
--- Process.java
+++ Process.java
@@ -4,37 +4,43 @@ import java.util.*;
 
 public class Process{
     /**Sophia S. Last Modified Date: 10/25/21 */
-   private int pnum; //process number
+   private int pid; //process number
    private int burst;
+   private int iburst; //i/o burst time
    private int arrival;
    private int exit; //exit time
    private int pri; //priority
 
    public Process(){
        /**Default constructor */
-       /**Sophia S. Last Modified Date: 10/22/21 */
-       this.pnum = 0;
+       /**Sophia S. Last Modified Date: 10/26/21 */
+       this.pid = 0;
        this.burst = 0;
+       this.iburst = 0;
        this.arrival = 0;
        this.exit = 0;
        this.pri = 0;
    }
 
-   public Process(int p, int b, int a, int e, int c){
-       /**Sophia S. Last Modified Date: 10/25/21 */
-       this.pnum = p;
+   public Process(int p, int b, int i, int a, int e, int c){
+       /**Sophia S. Last Modified Date: 10/26/21 */
+       this.pid = p;
        this.burst = b;
+       this.iburst = i;
        this.arrival = a;
        this.exit = e;
        this.pri = c; //i ran out of variables lol
    }
    /**All gets and sets:
-   /**Sophia S. Last Modified Date: 10/25/21 */ 
-   public int getPnum(){
-       return pnum;
+   /**Sophia S. Last Modified Date: 10/26/21 */ 
+   public int getPid(){
+       return pid;
    }
    public int getBurst(){
        return burst;
+   }
+   public int getiBurst(){
+       return iburst;
    }
    public int getArr(){
        return arrival;
@@ -45,11 +51,14 @@ public class Process{
    public int getPri(){
        return pri;
    }
-   public void setPnum(int p){
-       this.pnum = p;
+   public void setPid(int p){
+       this.pid = p;
    }
    public void setBurst(int b){
        this.burst = b;
+   }
+   public void setiBurst(int i){
+       this.iburst = i;
    }
    public void setArr(int a){
        this.arrival = a;
@@ -61,9 +70,10 @@ public class Process{
        this.pri = p;
    }
    public void printProcess(){
-       System.out.println("Process"+pnum+": ");
+       System.out.println("Process"+pid+": ");
        System.out.println("Arrival: "+arrival);
        System.out.println("Burst: "+burst);
+       System.out.println("I/O Burst: "+iburst);
        System.out.println("Exit: "+exit);
        System.out.println("Priority: "+pri);
    }
