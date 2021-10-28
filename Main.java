7306a0f777cc97559ad00a4ea645230d3e249f21
Sophia S <spswanberg44@gmail.com>
2021-10-28 12:25:01 -0500

    runq created, runningq implementation began

--- Main.java
+++ Main.java
@@ -4,20 +4,27 @@ import java.lang.Math;
 /*
 Group 8
 Sophia S
-Jessica B
+Jessica B 
 Alex M
 George O
 */
 
 public class main{
     public static void main(String[] args){
+        /**Initializations */
+        List<Process> hold = new ArrayList<Process>(); //new processes stored here
+        List<Process> ihandle = new ArrayList<Process>(); //this is the interrupt handler 
+        LinkedList<Process> newq = new LinkedList<Process>(); //final sorted processes go here
+        LinkedList<Process> runq = new LinkedList<Process>(); //running queue
+        int priority = 0;
+        int count = 0;
+        int ctime = 0; //current time
+        int quantum = 4; //time quantum 
+        /**------------------------------------------------------------------------ */
         /**Step 1: Create Processes */
         /**CPU Burst times should be in range 1-10 */
         /**I/O Burst times should be in range 11-21 */
         /**Sophia S. Last Modified Date: 10/26/21 */
-        List<Process> hold = new ArrayList<Process>(); //new processes stored here
-        LinkedList<Process> newq = new LinkedList<Process>(); //final sorted processes go here
-        int priority = 0;
         /**Generates the random arrival/burst/io burst */
         for(int i = 0; i < 4; i++){
             int arrival = (int)(Math.random()*6);
@@ -27,7 +34,7 @@ public class main{
             hold.add(p);
         }
         /**This is strictly for testing------------------------- */
-        /*Process p1 = new Process(1, 5, 10, 0, 0, 0);
+        /*Process p1 = new Process(1, 4, 10, 0, 0, 0);
         Process p2 = new Process(2, 5, 10, 0, 0, 0);
         Process p3 = new Process(3, 6, 14, 3, 0, 0);
         Process p4 = new Process(4, 2, 15, 2, 0, 0);
@@ -60,35 +67,79 @@ public class main{
         }
         System.out.println("Newq: ");
         for(int i = 0; i < newq.size(); i++){
-                System.out.println("Process "+ newq.get(i).getPid() + " Pri: " + newq.get(i).getPri() +" ");
+                System.out.println("Process "+ newq.get(i).getPid() + " Pri: " + newq.get(i).getPri() +" B: " + newq.get(i).getBurst() +" ");
         }
         /**------------------------------------------------------------------------------ */
-        /**Step 3: Enter the ready queue - George */
+        /**Step 3: Enter the ready queue */
         /**We need to determine the size of the ready queue*/
 
         /**Step 4: The first process in line will get the CPU */
 
+        /**----------------------------------------------------------------- */
         /**Step 5: Run the process in the time quantum.
             Finished? Move to terminated queue
-            Still working? Move to waiting queue to process I/O wait */
-        
-        /**New list:
+            Still working? Move to waiting queue to process I/O wait (lines 82-122)*/
+        /*System.out.println("Running queue implementation: ");
+        //ihandle.add(readyq.getFirst()); //uncomment when readyq is done
+        ihandle.add(newq.getFirst()); //testing
+        runq.add(ihandle.get(0));
+        System.out.println("Before: " + runq.getFirst().getBurst());
+        int burst = runq.getFirst().getBurst();
+        while(count < quantum){
+            if(burst == 0){
+                count = quantum;
+            }
+            else{
+                burst -= 1;
+                ctime += 1;
+            }
+            count += 1;
+        }
+        if(burst == 0){
+            System.out.println("burst is 0");
+            runq.getFirst().setExit(ctime);
+            //termq.add(runq.getFirst()); //uncomment when terminated queue is made
+            //System.out.println("Termq: " + termq.getFirst().getPid());
+            runq.getFirst().setBurst(burst);
+            System.out.println("After: " + runq.getFirst().getBurst() + " Exit: " + runq.getFirst().getExit());
+            System.out.println("ctime: " + ctime);
+        }
+        else{
+            System.out.println("burst is not 0");
+            runq.getFirst().setBurst(burst);
+            System.out.println("After: " + runq.getFirst().getBurst() + " Exit: " + runq.getFirst().getExit());
+            System.out.println("ctime: " + ctime);
+            /*if(runq.getFirst().getiBurst() == 0){
+                //CPU bound
+                readyq.addLast(runq.getFirst());
+                runq.clear(); //clears the running queue 
+            }
+            else{
+                //io bound
+                waitq.addLast(runq.getFirst());
+                runq.clear(); //clears the running queue
+            }
+        }*/
+        /**----------------------------------------------------------------- */
+        /**New list: Completed
         All newly SORTED processes will go here. */
 
-        /**Ready list:
+        /**Ready list: Incomplete
+        This will be done by George
         All CPU bound processess will be in this queue. The head of this list will be moved into 
         the running queue. */
 
-        /**Running list:
+        /**Running list: In progress
+        Sophia 
         This is where the process burst time will be decremented. If the time quantum is 4, then the 
         process will have (burst time - 4) 
         Finished? Record exit time*/
 
-        /**Waiting list:
+        /**Waiting list: Incomplete
         This is where the process will go when it's not finished executing. It will stay in the wait queue
         until it's I/O burst time variable has been decrememented to 0. */
 
-        /**Terminated list:
+        /**Terminated list: Incomplete
         To enter this list, process burst time must be 0 and the process exit time must be recorded */
 
         /**Notes:
